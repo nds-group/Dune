adfa80d8dd0a6bad65ed6d68e2e3eefc6d1d7020
ddeandres <david.deandres@imdea.org>
2024-06-27 10:43:10 +0200

    refactor to use multiprocessing lib

---
+++ UNSW/src/model_analysis/modelAnalyzer.py
@@ -0,0 +1,316 @@
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+
+import warnings
+
+class ModelAnalyzer:
+    def __init__(self, train_data_folder_path, test_data_folder_path, flow_counts_file_path, classes_filter,
+                 cluster_data_file_path):
+        self.cluster_data_file_path = cluster_data_file_path
+        self.number_of_clusters = pd.read_csv(cluster_data_file_path).shape[0]
+        self.train_data_folder_path = train_data_folder_path
+        self.test_data_folder_path = test_data_folder_path
+        self.flow_counts_file_path = flow_counts_file_path
+        self.classes_filter = classes_filter
+        self.feats_important = None
+        self.classes = None
+        self.classes_df = None
+        warnings.filterwarnings("ignore")
+        pd.options.mode.chained_assignment = None
+
+    def prepare_data(self, npkts, classes_filter=None):
+        def assign_sample_nature(row):
+            """Aux function to check the conditions and assign values"""
+            if (row["Min Packet Length"] == -1 and
+                    row["Max Packet Length"] == -1 and
+                    row["Flow IAT Min"] == -1 and
+                    row["Flow IAT Max"] == -1):
+                return "pkt"
+            else:
+                return "flw"
+
+        # Load Train and Test data
+        train_data = pd.read_csv(f"{self.train_data_folder_path}/train_data_{npkts}.csv")
+        test_data = pd.read_csv(f"{self.test_data_folder_path}/16-10-05.pcap.txt_{npkts}_pkts.csv")
+        if classes_filter is not None:
+            train_data = train_data.loc[train_data['Label'].isin(classes_filter)]
+            test_data = test_data.loc[test_data['Label'].isin(classes_filter)]
+
+        flow_pkt_counts = pd.read_csv(self.flow_counts_file_path)
+
+        flow_count_dict = flow_pkt_counts.set_index("flow.id")["count"].to_dict()
+        # Map the values from flow_pkt_counts to test_data based on the "Flow ID" column
+        test_data["pkt_count"] = test_data["Flow ID"].map(flow_count_dict)
+
+        #### To get packet count of each flow in train data
+        packet_data = pd.read_csv(f'{self.train_data_folder_path}/UNSW_train_ALL_PKT_DATE.csv')
+        packet_data = packet_data[['Flow ID', 'packet_count', 'File']]
+        packet_data['File_ID'] = packet_data['Flow ID'] + ' ' + packet_data['File']
+        packet_data = packet_data.drop_duplicates(subset='File_ID', keep='first')
+        train_data['File_ID'] = train_data['Flow ID'] + ' ' + train_data['File']
+
+        flow_count_dict_train = packet_data.set_index("File_ID")["packet_count"].to_dict()
+        # Map the values from flow_pkt_counts to test_data based on the "Flow ID" column
+        train_data["pkt_count"] = train_data["File_ID"].map(flow_count_dict_train)
+
+        all_minus_one = (test_data['Min Packet Length'] == -1) & (test_data['Max Packet Length'] == -1) & (
+                test_data['Packet Length Mean'] == -1)
+        # Assign values to the multiply column based on the conditions
+        test_data['multiply'] = np.where(all_minus_one, 1, test_data['pkt_count'] - npkts)
+
+        train_data = train_data.sample(frac=1, random_state=42)
+        test_data = test_data.sample(frac=1, random_state=42)
+
+        train_data = train_data.dropna(subset=['srcport', 'dstport'])
+        test_data = test_data.dropna(subset=['srcport', 'dstport'])
+
+        train_data['Label_NEW'] = np.where((train_data['Label'].isin(self.classes)), train_data['Label'], 'Other')
+        test_data['Label_NEW'] = np.where((test_data['Label'].isin(self.classes)), test_data['Label'], 'Other')
+        print(train_data['Label_NEW'].value_counts())
+        print(test_data['Label_NEW'].value_counts())
+
+        train_data['sample_nature'] = train_data.apply(assign_sample_nature, axis=1)
+        test_data['sample_nature'] = test_data.apply(assign_sample_nature, axis=1)
+
+        train_data['weight'] = np.where(train_data['sample_nature'] == 'flw',
+                                        (train_data['pkt_count'] - npkts + 1) / train_data['pkt_count'],
+                                        1 / train_data['pkt_count'])
+        return train_data, test_data
+
+    def analyze_models(self, classes, model_type, depths, n_trees, X_train, y_train, X_test, y_test, samples_nature,
+                       y_multiply, test_flow_pkt_cnt, test_flow_IDs, max_leaf, test_labels, test_indices, filename,
+                       weight_of_samples):
+        # open file to save ouput of analysis
+        with open(filename, "w") as res_file:
+            print(
+                'depth;tree;no_feats;N_Leaves;Macro_f1_FL;Weighted_f1_FL;Micro_f1_FL;feats;pkt_macro_f1'
+                ';pkt_weighted_f1;flw_macro_f1;flw_weighted_f1;F1_macro;F1_weighted;num_samples;Macro_F1_PL'
+                ';Weighted_F1_PL;Micro_F1_PL;cl_report_FL;cl_report_PL',
+                file=res_file)
+            if model_type == 'RF':
+                # FOR EACH (depth, n_tree, feat)
+                for n_tree in n_trees:
+                    # for depth in depths:
+                    depth = depths
+                    for leaf in max_leaf:
+                        # get feature orders to use
+                        importance = get_feature_importance(depth, n_tree, leaf, X_train, y_train, weight_of_samples)
+                        m_feats = get_fewest_features(depth, n_tree, leaf, importance)
+                        for feats in m_feats:
+                            # Get the scores with the given (depth, n_tree, feat)
+                            model, c_report, macro_f1, weight_f1, y_pred = get_scores(classes, depth, n_tree, feats,
+                                                                                      leaf, X_train, y_train, X_test,
+                                                                                      y_test, test_indices, test_labels,
+                                                                                      weight_of_samples)
+                            #
+                            pkt_macro_f1, pkt_weighted_f1, flw_macro_f1, flw_weighted_f1 = compute_flow_pkt_scores(
+                                y_pred,
+                                y_test,
+                                samples_nature,
+                                test_indices,
+                                test_labels)
+                            #
+                            num_samples, macro_f1_PL, weighted_f1_PL, micro_f1_PL, cl_report_PL, macro_f1_FL, weighted_f1_FL, micro_f1_FL, cl_report_FL = expand_rows_and_get_scores(
+                                y_test, y_pred, samples_nature, y_multiply, test_flow_pkt_cnt, test_flow_IDs,
+                                test_indices,
+                                test_labels)
+                            #
+                            depth = [estimator.tree_.max_depth for estimator in model.estimators_]
+                            print(str(depth) + ';' + str(n_tree) + ';' + str(len(feats)) + ';' + str(leaf) + ";" + str(
+                                macro_f1_FL) + ";" + str(weighted_f1_FL) + ";" + str(micro_f1_FL) + ";" + str(
+                                list(feats)) + ';' + str(pkt_macro_f1) + ';' + str(pkt_weighted_f1) + ';' + str(
+                                flw_macro_f1) + ';' + str(flw_weighted_f1) + ';' + str(macro_f1) + ';' + str(
+                                weight_f1) + ';' + str(num_samples) + ';' + str(macro_f1_PL) + ';' + str(
+                                weighted_f1_PL) + ';' + str(micro_f1_PL) + ';' + str(cl_report_FL) + ';' + str(
+                                cl_report_PL), file=res_file)
+        print("Analysis Complete. Check output file.")
+        return []
+
+    #
+    def analyze_model_n_packets(self, npkts, outfile):
+        """Function for grid search on hyperparameters, features and models"""
+        # Get Variables and Labels
+        train_data, test_data = self.prepare_data(npkts, self.classes_filter)
+
+        test_labels, test_indices = self.get_test_labels(test_data)
+        print("Num Labels: ", len(test_labels))
+
+        weight_of_samples = list(train_data['weight'])
+
+        y_multiply = test_data['multiply'].astype(int)
+        test_flow_pkt_cnt = test_data['pkt_count'].to_list()
+        test_flow_IDs = test_data['Flow ID'].to_list()
+        X_train, y_train, sample_nat_train = self.get_x_y_flow(train_data, self.feats_important)
+        X_test, y_test, sample_nat_test = self.get_x_y_flow(test_data, self.feats_important)
+
+        depths = []
+        trees = [1, 2, 3, 4, 5]
+        # Max values per TCAM table, i.e., 85 would require 2 TCAM tables
+        val_of_max_leaves = [41, 85, 129, 173, 217, 261, 305, 349, 393, 437, 481, 500]
+
+        #
+        self.analyze_models(self.classes, "RF", depths, trees, X_train, y_train, X_test, y_test,
+                            sample_nat_test, y_multiply, test_flow_pkt_cnt, test_flow_IDs, val_of_max_leaves,
+                            test_labels, test_indices, outfile, weight_of_samples)
+
+    def load_cluster_data(self, cluster_data_series):
+        # classes_str = str(pd.read_csv(cluster_data_file_path)['Class List'].to_list()[cluster_id])[2:-2]
+        # classes = classes_str.split("', '")
+        # classes.append('Other')
+        classes = list(set(cluster_data_series['Class List']))
+        classes.append('Other')
+        print(f'Cluster ID: {cluster_data_series["Cluster"]}', classes)
+        #
+        classes_df = pd.DataFrame(classes, columns=['class'])
+        classes_df = classes_df.reset_index()
+        #
+        # feats_important = pd.read_csv(cluster_data_file_path)['Feature List'].to_list()[cluster_id][2:-2].split("', '")
+        feats_important = cluster_data_series['Feature List']
+        print(f'Cluster ID: {cluster_data_series["Cluster"]}', feats_important)
+        self.feats_important = feats_important
+        self.classes = classes
+        self.classes_df = classes_df
+
+    def get_test_labels(self, test_data):
+        array_of_indices = []
+        unique_labels = test_data["Label_NEW"].unique()
+        for lab in unique_labels:
+            index = self.classes_df[self.classes_df['class'] == lab].index.values[0]
+            array_of_indices.append(index)
+        return unique_labels, array_of_indices
+
+    def get_x_y_flow(self, Dataset, feats):
+        X = Dataset[feats]
+        y = Dataset['Label_NEW'].replace(self.classes, range(len(self.classes)))
+        sample_nature = Dataset['sample_nature']
+        return X, y, sample_nature
+
+def get_feature_importance(depth, n_tree, max_leaf, X_train, y_train, weight_of_samples):
+    """
+    Function to Fit model based on optimal values of depth and number of estimators and use it
+    to compute feature importance for all the features.
+    """
+    rf_opt = RandomForestClassifier(n_estimators=n_tree, max_leaf_nodes=max_leaf, random_state=42, bootstrap=False,
+                                    n_jobs=10)
+    rf_opt.fit(X_train, y_train, sample_weight=weight_of_samples)
+    feature_importance = pd.DataFrame(rf_opt.feature_importances_)
+    feature_importance.index = X_train.columns
+    feature_importance = feature_importance.sort_values(by=list(feature_importance.columns), axis=0, ascending=False)
+
+    return feature_importance
+
+def get_fewest_features(depth, n_tree, max_leaf, importance):
+    """
+    Function to Fit model based on optimal values of depth and number of estimators and feature importance
+    to find the fewest possible features to exceed the previously attained score with all selected features
+    """
+    sorted_feature_names = importance.index
+    features = []
+    for f in range(1, len(sorted_feature_names) + 1):
+        features.append(sorted_feature_names[0:f])
+    return features
+
+
+def get_scores(classes, depth, n_tree, feats, max_leaf, X_train, y_train, X_test, y_test, unique_labels,
+               array_of_indices, weight_of_samples):
+    model = RandomForestClassifier(n_estimators=n_tree, max_leaf_nodes=max_leaf, n_jobs=10,
+                                   random_state=42, bootstrap=False)
+
+    model.fit(X_train[feats], y_train, sample_weight=weight_of_samples)
+    y_pred = model.predict(X_test[feats])
+
+    y_test = [int(label) for label in y_test.values]
+    y_pred = [int(label) for label in y_pred]
+
+    class_report = classification_report(y_test, y_pred, labels=unique_labels, target_names=array_of_indices,
+                                         output_dict=True)
+
+    macro_score = class_report['macro avg']['f1-score']
+    weighted_score = class_report['weighted avg']['f1-score']
+
+    return model, class_report, macro_score, weighted_score, y_pred
+
+
+def expand_rows_and_get_scores(y_true, y_pred, sample_nature, multiply, test_flow_pkt_cnt, test_flow_IDs, unique_labels,
+                               array_of_indices):
+    expanded_y_true = []
+    expanded_y_pred = []
+    #
+    expanded_weights = []
+    expanded_flow_IDs = []
+
+    for true_label, pred_label, nature, mult, pkt_cnt, f_id in zip(y_true, y_pred, sample_nature, multiply,
+                                                                   test_flow_pkt_cnt, test_flow_IDs):
+        if nature == 'flw':
+            expanded_y_true.extend([true_label] * (mult + 1))
+            expanded_y_pred.extend([pred_label] * (mult + 1))
+            #
+            expanded_weights.extend([1 / pkt_cnt] * (mult + 1))
+            expanded_flow_IDs.extend([f_id] * (mult + 1))
+        else:
+            expanded_y_true.append(true_label)
+            expanded_y_pred.append(pred_label)
+            #
+            expanded_weights.append(1 / pkt_cnt)
+            expanded_flow_IDs.append(f_id)
+
+    num_samples = len(expanded_y_true)
+
+    expanded_y_true = [int(label) for label in expanded_y_true]
+    expanded_y_pred = [int(label) for label in expanded_y_pred]
+    cl_report_PL = classification_report(expanded_y_true, expanded_y_pred, labels=unique_labels,
+                                         target_names=array_of_indices, output_dict=True)
+    macro_f1_PL = cl_report_PL['macro avg']['f1-score']
+    weighted_f1_PL = cl_report_PL['weighted avg']['f1-score']
+    try:
+        micro_f1_PL = cl_report_PL['micro avg']['f1-score']
+    except:
+        micro_f1_PL = cl_report_PL['accuracy']
+    ####
+
+    c_report_FL = classification_report(expanded_y_true, expanded_y_pred, labels=unique_labels,
+                                        target_names=array_of_indices, output_dict=True, sample_weight=expanded_weights)
+
+    macro_f1_FL = c_report_FL['macro avg']['f1-score']
+    weighted_f1_FL = c_report_FL['weighted avg']['f1-score']
+    try:
+        micro_f1_FL = c_report_FL['micro avg']['f1-score']
+    except:
+        micro_f1_FL = c_report_FL['accuracy']
+
+    return num_samples, macro_f1_PL, weighted_f1_PL, micro_f1_PL, cl_report_PL, macro_f1_FL, weighted_f1_FL, micro_f1_FL, c_report_FL
+
+
+def compute_flow_pkt_scores(y_pred, y_test, sample_nature, unique_labels, array_of_indices):
+    # Create a data frame with the three columns
+    df = pd.DataFrame({'y_pred': y_pred, 'y_test': y_test, 'sample_nature': sample_nature})
+
+    # Split the data frame into two data frames based on sample_nature
+    pkt_df = df[df['sample_nature'] == 'pkt']
+    flw_df = df[df['sample_nature'] == 'flw']
+
+    # Compute macro and weighted F1 scores for pkt_df
+    pkt_df_y_true = [int(label) for label in pkt_df['y_test'].values]
+    pkt_df_y_pred = [int(label) for label in pkt_df['y_pred']]
+
+    pkt_macro_f1 = \
+    classification_report(pkt_df_y_true, pkt_df_y_pred, labels=unique_labels, target_names=array_of_indices,
+                          output_dict=True)['macro avg']['f1-score']
+    pkt_weighted_f1 = \
+    classification_report(pkt_df_y_true, pkt_df_y_pred, labels=unique_labels, target_names=array_of_indices,
+                          output_dict=True)['weighted avg']['f1-score']
+
+    # Compute macro and weighted F1 scores for flw_df
+    flw_df_y_true = [int(label) for label in flw_df['y_test'].values]
+    flw_df_y_pred = [int(label) for label in flw_df['y_pred']]
+
+    flw_macro_f1 = \
+    classification_report(flw_df_y_true, flw_df_y_pred, labels=unique_labels, target_names=array_of_indices,
+                          output_dict=True)['macro avg']['f1-score']
+    flw_weighted_f1 = \
+    classification_report(flw_df_y_true, flw_df_y_pred, labels=unique_labels, target_names=array_of_indices,
+                          output_dict=True)['weighted avg']['f1-score']
+
+    return pkt_macro_f1, pkt_weighted_f1, flw_macro_f1, flw_weighted_f1
---
+++ UNSW/src/run_cluster_analysis.py
@@ -0,0 +1,58 @@
+from model_analysis.modelAnalyzer import ModelAnalyzer
+from multiprocessing import Pool
+from ast import literal_eval
+from itertools import product
+import pandas as pd
+
+classes_filter = ['Amazon Echo', 'Android Phone', 'Belkin Wemo switch', 'Belkin wemo motion sensor', 'Dropcam', 'HP ',
+                  'Printer', 'Insteon Camera', 'Laptop', 'Light Bulbs LiFX Smart Bulb', 'MacBook',
+                  'NEST Protect smoke alarm', 'Netatmo Welcome', 'Netatmo weather station', 'PIX-STAR Photo-frame',
+                  'Samsung Galaxy Tab', 'Samsung SmartCam', 'Smart Things', 'TP-Link Day Night Cloud camera',
+                  'TP-Link Smart plug', 'Triby Speaker']
+
+train_data_dir_path = '/home/nds-admin/UNSW_PCAPS/train/train_data_hybrid'
+test_data_dir_path = '/home/nds-admin/UNSW_PCAPS/test/csv_files'
+flow_counts_file_path = '/home/nds-admin/UNSW_PCAPS/hyb_code/16-10-05-flow-counts.csv'
+cluster_data_file_path = '/home/ddeandres/distributed_in_band/UNSW/cluster_info/UNSW_SPP_solution.csv'
+results_dir_path = '/home/ddeandres/distributed_in_band/UNSW/cluster_model_analysis_results/test'
+
+
+def literal_converter(val):
+    # replace first val with '' or some other null identifier if required
+    return val if val == '' else literal_eval(val)
+
+
+cluster_info = pd.read_csv(cluster_data_file_path,
+                           converters=dict.fromkeys(['Class List', 'Feature List'], literal_converter))
+
+
+# def run_analysis(n_point, cluster_id):
+#     print(f"Starting analysis of: Cluster id: {cluster_id}, npoint {n_point}")
+#     f_name = f"{results_dir_path}/unsw_models_{n_point}pkts_PF_WB_20CL_Cluster{cluster_id}.csv"
+#     model_analyzer = ModelAnalyzer(train_data_dir_path, test_data_dir_path, flow_counts_file_path,
+#                                    classes_filter, cluster_data_file_path)
+#     model_analyzer.load_cluster_data(cluster_info.loc[cluster_id])
+#     model_analyzer.analyze_model_n_packets(n_point, f_name)
+#     print(f"Finished analyzing n={n_point}, Cluster={cluster_id}. Results at: {results_dir_path}")n
+
+def run_analysis(input_data):
+    n_point = input_data[0]
+    cluster_id = input_data[1]
+    print(f"Starting analysis of: Cluster id: {cluster_id}, npoint {n_point}")
+    f_name = f"{results_dir_path}/unsw_models_{n_point}pkts_PF_WB_20CL_Cluster{cluster_id}.csv"
+    model_analyzer = ModelAnalyzer(train_data_dir_path, test_data_dir_path, flow_counts_file_path,
+                                   classes_filter, cluster_data_file_path)
+    model_analyzer.load_cluster_data(cluster_info.loc[cluster_id])
+    model_analyzer.analyze_model_n_packets(n_point, f_name)
+    print(f"Finished analyzing n={n_point}, Cluster={cluster_id}. Results at: {results_dir_path}")
+
+
+inference_points_list = list(range(2, 5))
+cluster_id_list = cluster_info['Cluster'].to_list()
+
+with Pool(processes=24) as pool:
+    for result in pool.imap_unordered(run_analysis, list(product(inference_points_list,cluster_id_list))):
+        pass # or do something with result, if pool_tasks returns a value
+#     # pool.starmap(run_analysis, zip(list(range(2, 5)), list(range(3))), chunksize=1)
+
+# run_analysis(list(zip(list(range(2, 5)), list(range(3))))[0])
